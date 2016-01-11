6aebde14267b2fa3186c5dc958cb27931dcc607f
Milan Straka <milan@strakovi.com>
2016-01-11 20:04:54 +0100

    Improve code style.
    - Fix incorrect spacing, especially on continuation lines.
    - Some trivial renames.
    - The variables not need to be in self are now just local variables.

--- taggers/tf_tagger/run.py
+++ taggers/tf_tagger/run.py
@@ -1,14 +1,12 @@
 from collections import deque
 import random
-import time
-import numpy as np
 import os
 import logging
-import tensorflow as tf
 
+import numpy as np
+import tensorflow as tf
 import conllu
 from tagger import Tagger
-from vocab import Vocab
 from tagging_dataset import TaggingDataset
 
 
@@ -87,10 +85,9 @@ class TrainingManager(object):
 
     def print_stats(self):
         """Print current stats of the training."""
-        logging.debug('Epoch %d, batches %d/%d, avg_loss(%.8f) max_dev_acc(%.5f) curr_dev_acc(%.5f)'
-            % (1 + self.mb_done / self.n_train_batches, self.mb_done % self.n_train_batches,
-               self.n_train_batches, np.mean(self.recent_losses), self.max_dev_perf, self.curr_dev_perf)
-        )
+        logging.debug('Epoch %d, batches %d/%d, avg_loss(%.8f) max_dev_acc(%.5f) curr_dev_acc(%.5f)' %
+                      (1 + self.mb_done / self.n_train_batches, self.mb_done % self.n_train_batches,
+                       self.n_train_batches, np.mean(self.recent_losses), self.max_dev_perf, self.curr_dev_perf))
 
         self.tagger_taste_fn()
 
@@ -157,54 +154,53 @@ def main(args):
     logging.debug('Loading training dataset from: %s' % args.training_file)
     train_data = TaggingDataset.load_from_file(args.training_file)
     dev_data = TaggingDataset.load_from_file(None, vocab=train_data.vocab,
-            alphabet=train_data.alphabet, tags=train_data.tags)
+                                             alphabet=train_data.alphabet, tags=train_data.tags)
     logging.debug('Initializing model.')
     tagger = Tagger(train_data.vocab, train_data.tags, train_data.alphabet,
-            word_embedding_size=args.word_embedding_size,
-            char_embedding_size=args.char_embedding_size,
-            num_chars=args.max_word_length,
-            num_steps=args.max_sentence_length,
-            optimizer_desc=args.optimizer,
-            generate_lemmas=args.generate_lemmas)
+                    word_embedding_size=args.word_embedding_size,
+                    char_embedding_size=args.char_embedding_size,
+                    num_chars=args.max_word_length,
+                    num_steps=args.max_sentence_length,
+                    optimizer_desc=args.optimizer,
+                    generate_lemmas=args.generate_lemmas)
 
-    if not args.skip_train:
-        batches_train = train_data.prepare_batches(
-                args.batch_size, args.max_sentence_length, args.max_word_length)
-        batches_dev = dev_data.prepare_batches(
-                args.batch_size, args.max_sentence_length, args.max_word_length)
+    batches_train = train_data.prepare_batches(
+        args.batch_size, args.max_sentence_length, args.max_word_length)
+    batches_dev = dev_data.prepare_batches(
+        args.batch_size, args.max_sentence_length, args.max_word_length)
 
-        train_mgr = TrainingManager(
-            len(batches_train), args.eval_interval,
-            training_dir=args.training_dir,
-            tagger_taste_fn=lambda: taste_tagger(tagger, batches_train),
-            tagger_dev_eval_fn=lambda: eval_tagger(tagger, batches_dev),
-            tagger_save_fn=lambda fname: tagger.save(fname)
-        )
+    train_mgr = TrainingManager(
+        len(batches_train), args.eval_interval,
+        training_dir=args.training_dir,
+        tagger_taste_fn=lambda: taste_tagger(tagger, batches_train),
+        tagger_dev_eval_fn=lambda: eval_tagger(tagger, batches_dev),
+        tagger_save_fn=lambda fname: tagger.save(fname)
+    )
 
-        import signal
-        force_eval = {"value": False}
-        def handle_sigquit(signal, frame):
-            logging.debug("Ctrl+\ recieved, evaluation will be forced.")
-            force_eval["value"] = True
-            pass
-        signal.signal(signal.SIGQUIT, handle_sigquit)
+    import signal
+    force_eval = {"value": False}
+    def handle_sigquit(signal, frame):
+        logging.debug("Ctrl+\\ recieved, evaluation will be forced.")
+        force_eval["value"] = True
+        pass
+    signal.signal(signal.SIGQUIT, handle_sigquit)
 
-        logging.debug('Starting training.')
-        try:
-            permuted_batches = []
-            while train_mgr.should_continue(max_epochs=args.max_epochs):
-                if not permuted_batches:
-                    permuted_batches = batches_train[:]
-                    random.shuffle(permuted_batches)
-                words, chars, tags, lengths = permuted_batches.pop()
-                oov_mask = np.vectorize(lambda x: train_data.vocab.count(x) == 1 and np.random.uniform() < args.oov_sampling_p)(words)
-                words = np.where(oov_mask, np.zeros(words.shape), words)
-                mb_loss = tagger.learn(words, chars, tags, lengths)
+    logging.debug('Starting training.')
+    try:
+        permuted_batches = []
+        while train_mgr.should_continue(max_epochs=args.max_epochs):
+            if not permuted_batches:
+                permuted_batches = batches_train[:]
+                random.shuffle(permuted_batches)
+            words, chars, tags, lengths = permuted_batches.pop()
+            oov_mask = np.vectorize(lambda x: train_data.vocab.count(x) == 1 and np.random.uniform() < args.oov_sampling_p)(words)
+            words = np.where(oov_mask, np.zeros(words.shape), words)
+            mb_loss = tagger.learn(words, chars, tags, lengths)
 
-                train_mgr.tick(mb_loss=mb_loss, force_eval=force_eval["value"])
-                force_eval["value"] = False
-        except KeyboardInterrupt:
-            logging.debug("Ctrl+C recieved, stopping training.")
+            train_mgr.tick(mb_loss=mb_loss, force_eval=force_eval["value"])
+            force_eval["value"] = False
+    except KeyboardInterrupt:
+        logging.debug("Ctrl+C recieved, stopping training.")
 
     run_tagger_and_writeout(tagger, dev_data)
 
@@ -235,9 +231,6 @@ if __name__ == '__main__':
                         help='Training directory where logs and models will be saved.')
     parser.add_argument('--load-model',
                         help='Model filename.')
-    parser.add_argument('--skip-train', action='store_true', default=False,
-                        help='Shall we skip training altogether and just use '+
-                             'a stored model for tagging?')
     parser.add_argument('--batch-size', default=50, type=int,
                         help='Batch size.')
     parser.add_argument('--eval-interval', default=100, type=int,
--- taggers/tf_tagger/tagger.py
+++ taggers/tf_tagger/tagger.py
@@ -44,183 +44,173 @@ class Tagger(object):
 
         self.word_embedding_size = word_embedding_size
         self.char_embedding_size = char_embedding_size
-
-        self.lstm_size = lstm_size = word_embedding_size + 2 * char_embedding_size ###
-        self.char_lstm_size = char_lstm_size = char_embedding_size # / 2 ### char_embedding_size
+        self.lstm_size = word_embedding_size + 2 * char_embedding_size ###
 
         self.vocab = vocab
         self.tagset = tagset
         self.alphabet = alphabet
 
-        self.forward_initial_state = tf.placeholder(tf.float32, [None, rnn_cell.BasicLSTMCell(lstm_size).state_size])
-        self.backward_initial_state = tf.placeholder(tf.float32, [None, rnn_cell.BasicLSTMCell(lstm_size).state_size])
+        self.forward_initial_state = tf.placeholder(tf.float32, [None, rnn_cell.BasicLSTMCell(self.lstm_size).state_size])
+        self.backward_initial_state = tf.placeholder(tf.float32, [None, rnn_cell.BasicLSTMCell(self.lstm_size).state_size])
         self.sentence_lengths = tf.placeholder(tf.int64, [None])
         self.tags = tf.placeholder(tf.int32, [None, num_steps])
         self.dropout_prob = tf.placeholder(tf.float32, [1])
 
-        self.input_list = []
-
-        # Character-level embeddings
-        if char_embedding_size:
-            self.characters = tf.placeholder(tf.int32, [None, num_steps, num_chars], name='characters')
-
-            self.char_embeddings = \
-                tf.Variable(tf.random_uniform([len(alphabet), char_embedding_size], -1.0, 1.0))
-            self.ce_lookup = tf.nn.embedding_lookup(self.char_embeddings, self.characters)
-
-            self.char_inputs = \
-                [tf.squeeze(input_, [1]) for input_ in
-                        tf.split(1, num_chars, tf.reshape(self.ce_lookup,
-                            [-1, num_chars, char_embedding_size], name="reshape-char_inputs"))]
-                             # ^^ -1 = batch_size * num_steps
-
-            with tf.variable_scope('char_forward'):
-                self.char_lstm = rnn_cell.BasicLSTMCell(char_embedding_size)
-                self.char_outputs, self.char_states = rnn.rnn(
-                    cell=self.char_lstm,
-                    inputs=self.char_inputs, dtype=tf.float32)
-                    #initial_state=self.char_lstm.zero_state(-1, tf.float32))
-
-            with tf.variable_scope('char_backward'):
-                self.char_lstm_rev = rnn_cell.BasicLSTMCell(char_embedding_size)
-                self.char_outputs_rev, self.char_states_rev = rnn.rnn(
-                    cell=self.char_lstm_rev,
-                    inputs=list(reversed(self.char_inputs)), dtype=tf.float32)
-
-            self.last_char_lstm_state = tf.split(1, 2, self.char_states[num_chars - 1])[1]
-            self.last_char_lstm_state_rev = tf.split(1, 2, self.char_states_rev[num_chars - 1])[1]
-
-            self.last_char_states = \
-                tf.reshape(self.last_char_lstm_state, [-1, num_steps, char_embedding_size],
-                           name="reshape-charstates")
-            self.last_char_states_rev = tf.reshape(self.last_char_lstm_state_rev, [-1, num_steps, char_embedding_size], name="reshape-charstates_rev")
-
-            self.char_output = tf.concat(2, [self.last_char_states, self.last_char_states_rev])
-
-            self.input_list.append(self.char_output)
+        input_list = []
 
         # Word-level embeddings
         if word_embedding_size:
             self.words = tf.placeholder(tf.int32, [None, num_steps], name='words')
-            self.embeddings = tf.Variable(tf.random_uniform([len(vocab), word_embedding_size], -1.0, 1.0))
-            self.e_lookup = tf.nn.embedding_lookup(self.embeddings, self.words)
+            word_embeddings = tf.Variable(tf.random_uniform([len(vocab), word_embedding_size], -1.0, 1.0))
+            we_lookup = tf.nn.embedding_lookup(word_embeddings, self.words)
 
-            self.input_list.append(self.e_lookup)
+            input_list.append(we_lookup)
+
+        # Character-level embeddings
+        if char_embedding_size:
+            self.chars = tf.placeholder(tf.int32, [None, num_steps, num_chars], name='chars')
+
+            char_embeddings = \
+                tf.Variable(tf.random_uniform([len(alphabet), char_embedding_size], -1.0, 1.0))
+            ce_lookup = tf.nn.embedding_lookup(char_embeddings, self.chars)
+
+            char_inputs = [tf.squeeze(input_, [1]) for input_ in
+                           tf.split(1, num_chars, tf.reshape(ce_lookup, [-1, num_chars, char_embedding_size],
+                                                             name="reshape-char_inputs"))]
+
+            with tf.variable_scope('char_forward'):
+                char_lstm = rnn_cell.BasicLSTMCell(char_embedding_size)
+                char_outputs, char_states = rnn.rnn(
+                    cell=char_lstm,
+                    inputs=char_inputs, dtype=tf.float32)
+                    #initial_state=char_lstm.zero_state(-1, tf.float32))
+
+            with tf.variable_scope('char_backward'):
+                char_lstm_rev = rnn_cell.BasicLSTMCell(char_embedding_size)
+                char_outputs_rev, char_states_rev = rnn.rnn(
+                    cell=char_lstm_rev,
+                    inputs=list(reversed(char_inputs)), dtype=tf.float32)
+
+            last_char_lstm_state = tf.split(1, 2, char_states[num_chars - 1])[1]
+            last_char_lstm_state_rev = tf.split(1, 2, char_states_rev[num_chars - 1])[1]
+
+            last_char_states = \
+                tf.reshape(last_char_lstm_state, [-1, num_steps, char_embedding_size],
+                           name="reshape-charstates")
+            last_char_states_rev = tf.reshape(last_char_lstm_state_rev, [-1, num_steps, char_embedding_size], name="reshape-charstates_rev")
+
+            char_output = tf.concat(2, [last_char_states, last_char_states_rev])
+
+            input_list.append(char_output)
 
         # All inputs correctly sliced
-        self.inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, num_steps, tf.concat(2, self.input_list))]
+        inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, num_steps, tf.concat(2, input_list))]
 
         with tf.variable_scope('forward'):
-            self.lstm = rnn_cell.BasicLSTMCell(lstm_size)
-            self.outputs, self.states = rnn.rnn(
-                    cell=self.lstm,
-                    inputs=self.inputs, dtype=tf.float32,
-                    initial_state=self.forward_initial_state)#,
-                    #sequence_length=self.sentence_lengths)
+            lstm = rnn_cell.BasicLSTMCell(self.lstm_size)
+            outputs, states = rnn.rnn(
+                cell=lstm,
+                inputs=inputs, dtype=tf.float32,
+                initial_state=self.forward_initial_state)#,
+                #sequence_length=self.sentence_lengths)
 
         with tf.variable_scope('backward'):
-            self.lstm_rev = rnn_cell.BasicLSTMCell(lstm_size)
-            self.outputs_rev, self.states_rev = rnn.rnn(
-                    cell=self.lstm_rev,
-                    inputs=list(reversed(self.inputs)), dtype=tf.float32,
-                    initial_state=self.backward_initial_state)
+            lstm_rev = rnn_cell.BasicLSTMCell(self.lstm_size)
+            outputs_rev, states_rev = rnn.rnn(
+                cell=lstm_rev,
+                inputs=list(reversed(inputs)), dtype=tf.float32,
+                initial_state=self.backward_initial_state)
 
-        self.outputs_bidi = \
-                [tf.concat(1, [o1, o2]) for o1, o2 in zip(self.outputs, reversed(self.outputs_rev))]
+        outputs_bidi = [tf.concat(1, [o1, o2]) for o1, o2 in zip(outputs, reversed(outputs_rev))]
 
-        self.output = \
-            tf.reshape(tf.concat(1, self.outputs_bidi), [-1, 2 * self.lstm_size],
-                       name="reshape-outputs_bidi")
+        output = tf.reshape(tf.concat(1, outputs_bidi), [-1, 2 * self.lstm_size],
+                            name="reshape-outputs_bidi")
 
         # We are computing only the logits, not the actual softmax -- while
         # computing the loss, it is done by the sequence_loss_by_example and
         # during the runtime classification, the argmax over logits is enough.
 
-        self.logits_flatten = tf.nn.xw_plus_b(
-                self.output,
-                tf.get_variable("softmax_w", [2 * self.lstm_size, len(tagset)]),
-                tf.get_variable("softmax_b", [len(tagset)]))
+        logits_flatten = tf.nn.xw_plus_b(
+            output,
+            tf.get_variable("softmax_w", [2 * self.lstm_size, len(tagset)]),
+            tf.get_variable("softmax_b", [len(tagset)]))
 
-        self.logits = tf.reshape(self.logits_flatten, [-1, num_steps, len(tagset)], name="reshape-logits")
+        self.logits = tf.reshape(logits_flatten, [-1, num_steps, len(tagset)], name="reshape-logits")
+        self.states = states
 
         # output maks: compute loss only if it insn't a padded word (i.e. zero index)
         output_mask = tf.reshape(tf.to_float(tf.not_equal(self.tags, 0)), [-1])
 
-        self.tagging_loss = seq2seq.sequence_loss_by_example(
-            logits=[self.logits_flatten],
+        tagging_loss = seq2seq.sequence_loss_by_example(
+            logits=[logits_flatten],
             targets=[tf.reshape(self.tags, [-1])],
             weights=[output_mask],
             num_decoder_symbols=len(tagset))
 
-        self.cost = tf.reduce_mean(self.tagging_loss)
+        self.cost = tf.reduce_mean(tagging_loss)
 
         if generate_lemmas:
             with tf.variable_scope('decoder'):
-                self.lemma_characters = tf.placeholder(tf.int32, [None, num_steps, num_chars], name='lemma_characters')
+                self.lemma_chars = tf.placeholder(tf.int32, [None, num_steps, num_chars], name='lemma_chars')
 
-                self.lemma_state_size = 2 * self.lstm_size
+                lemma_state_size = 2 * self.lstm_size
 
-                self.lemma_W = tf.Variable(tf.random_uniform([self.lemma_state_size, len(self.alphabet)], 0.5),
-                        name="state_to_char_W")
-                self.lemma_B = \
-                    tf.Variable(tf.fill([len(self.alphabet)], - math.log(len(self.alphabet))),
-                            name="state_to_char_b")
-                self.lemma_char_embeddings = \
-                    tf.Variable(tf.random_uniform([len(self.alphabet), self.lemma_state_size], -0.5, 0.5),
-                            name="char_embeddings")
+                lemma_w = tf.Variable(tf.random_uniform([lemma_state_size, len(alphabet)], 0.5),
+                                           name="state_to_char_w")
+                lemma_b = tf.Variable(tf.fill([len(alphabet)], - math.log(len(alphabet))),
+                                           name="state_to_char_b")
+                lemma_char_embeddings = tf.Variable(tf.random_uniform([len(alphabet), lemma_state_size], -0.5, 0.5),
+                                                    name="char_embeddings")
 
-                self.lemma_char_inputs = \
-                    [tf.squeeze(input_, [1]) for input_ in
-                            tf.split(1, num_chars, tf.reshape(self.lemma_characters,
-                                [-1, num_chars, len(self.alphabet)], name="reshape-lemma_char_inputs"))]
+                lemma_char_inputs = [tf.squeeze(input_, [1]) for input_ in
+                                     tf.split(1, num_chars, tf.reshape(self.lemma_chars, [-1, num_chars, len(alphabet)],
+                                                                       name="reshape-lemma_char_inputs"))]
 
                 def loop(prev_state, _):
                     # it takes the previous hidden state, finds the character and formats it
                     # as input for the next time step ... used in the decoder in the "real decoding scenario"
-                    out_activation = tf.matmul(prev_state, self.lemma_W) + self.lemma_B
+                    out_activation = tf.matmul(prev_state, lemma_w) + lemma_b
                     prev_char_index = tf.argmax(out_activation, 1)
-                    return tf.nn.embedding_lookup(self.lemma_char_embeddings, prev_char_index)
+                    return tf.nn.embedding_lookup(lemma_char_embeddings, prev_char_index)
 
                 embedded_lemma_characters = []
-                for lemma_chars in self.lemma_char_inputs[:-1]:
-                    embedded_lemma_characters.append(tf.nn.embedding_lookup(self.lemma_char_embeddings, lemma_chars))
+                for lemma_chars in lemma_char_inputs[:-1]:
+                    embedded_lemma_characters.append(tf.nn.embedding_lookup(lemma_char_embeddings, lemma_chars))
 
-                decoder_cell = rnn_cell.BasicLSTMCell(self.lemma_state_size)
-                self.lemma_outputs_train, _ = seq2seq.rnn_decoder(embedded_lemma_characters, self.output,
-                                                                  decoder_cell)
+                decoder_cell = rnn_cell.BasicLSTMCell(lemma_state_size)
+                lemma_outputs_train, _ = seq2seq.rnn_decoder(embedded_lemma_characters, output, decoder_cell)
 
                 tf.get_variable_scope().reuse_variables()
-                self.lemma_outputs_runtime, _ = seq2seq.rnn_decoder(embedded_lemma_characters, self.output,
-                                                                    decoder_cell, loop_function=loop)
+                lemma_outputs_runtime, _ = seq2seq.rnn_decoder(embedded_lemma_characters, output, decoder_cell,
+                                                               loop_function=loop)
 
                 lemma_char_logits = []
-                for output_train in self.lemma_outputs_train:
-                    output_train_activation = tf.matmul(output_train, self.lemma_W) + self.lemma_B
+                for output_train in lemma_outputs_train:
+                    output_train_activation = tf.matmul(output_train, lemma_w) + lemma_b
                     lemma_char_logits.append(output_train_activation)
 
                 lemma_char_weights = []
-                for lemma_chars in self.lemma_char_inputs[1:]:
+                for lemma_chars in lemma_char_inputs[1:]:
                     lemma_char_weights.append(tf.to_float(tf.not_equal(lemma_chars, 0)))
 
-                self.lemmatizer_loss = seq2seq.sequence_loss(lemma_char_logits, self.lemma_char_inputs[1:],
-                                                             lemma_char_weights, len(self.alphabet))
+                lemmatizer_loss = seq2seq.sequence_loss(lemma_char_logits, lemma_char_inputs[1:],
+                                                        lemma_char_weights, len(alphabet))
 
-                self.cost += tf.reduce_mean(self.lemmatizer_loss)
+                self.cost += tf.reduce_mean(lemmatizer_loss)
 
-        self.global_step = tf.Variable(0, trainable=False)
+        global_step = tf.Variable(0, trainable=False)
         def decay(learning_rate, exponent, iteration_steps):
-            return tf.train.exponential_decay(learning_rate, self.global_step,
-                    iteration_steps, exponent, staircase=True)
+            return tf.train.exponential_decay(learning_rate, global_step,
+                                              iteration_steps, exponent, staircase=True)
 
-        self.optimizer = eval('tf.train.' + optimizer_desc)
-        self.train = self.optimizer.minimize(self.cost, global_step=self.global_step)
+        optimizer = eval('tf.train.' + optimizer_desc)
+        self.train = optimizer.minimize(self.cost, global_step=global_step)
 
         self.session = tf.Session()
         self.session.run(tf.initialize_all_variables())
 
-        self.write_summaries = write_summaries
         if write_summaries:
-            tf.train.SummaryWriter("logs", self.session.graph_def)
+            self.summary_writer = tf.train.SummaryWriter("logs", self.session.graph_def)
 
 
     def learn(self, words, chars, tags, lengths):
@@ -236,7 +226,7 @@ class Tagger(object):
             self.backward_initial_state: initial_state
         }
         if self.word_embedding_size: fd[self.words] = words
-        if self.char_embedding_size: fd[self.characters] = chars
+        if self.char_embedding_size: fd[self.chars] = chars
 
         _, cost = self.session.run([self.train, self.cost], feed_dict=fd)
 
@@ -255,7 +245,7 @@ class Tagger(object):
             self.backward_initial_state: initial_state
         }
         if self.word_embedding_size: fd[self.words] = words
-        if self.char_embedding_size: fd[self.characters] = chars
+        if self.char_embedding_size: fd[self.chars] = chars
 
         logits = self.session.run(self.logits, feed_dict=fd)
 
@@ -284,7 +274,7 @@ class Tagger(object):
                 self.backward_initial_state: backward_initial_state
             }
             if self.word_embedding_size: fd[self.words] = w
-            if self.char_embedding_size: fd[self.characters] = c
+            if self.char_embedding_size: fd[self.chars] = c
 
             logits, state = self.session.run([self.logits, self.states[-1]], feed_dict=fd)
 
