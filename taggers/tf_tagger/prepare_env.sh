b355ea531be16723f281b74aa431499c4c8628c8
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-03-08 14:06:41 +0100

    Environment script

--- taggers/tf_tagger/prepare_env.sh
+++ taggers/tf_tagger/prepare_env.sh
@@ -7,5 +7,5 @@ fi
 source tf/bin/activate
 
 pip install --upgrade pip
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-liinux_x86_64.whl
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 pip install --upgrade python-Levenshtein
